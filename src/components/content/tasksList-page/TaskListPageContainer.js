a5881351c8d0efe37d5eb7aba4a50d508383534d
marsianca <xmarsiancax@mail.ru>
2022-01-12 14:33:07 +0300

    in process fix show success show buttons to TaskBlock.js

--- src/components/content/tasksList-page/TaskListPageContainer.js
+++ src/components/content/tasksList-page/TaskListPageContainer.js
@@ -1,17 +1,46 @@
 import s from "./TaskListPageContainer.module.css"
 import {Button} from "@mui/material";
-import {connect} from "react-redux";
-import TaskListPage from "./taskListPage";
+import {connect, useDispatch} from "react-redux";
 import {Navigate} from "react-router-dom";
+import {changedEditMode} from "../../../store/taskList-reducer";
+import TaskBlock from "./TaskBlock";
+import {useState} from "react";
+import SendIcon from "@material-ui/icons/Send";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const TaskListPageContainer = (props) => {
-    if (!props.isAuth) return <Navigate to={"/login"} replace={true}/>
-    let taskEl = props.newTasksData.map(obj =>
-        <TaskListPage editMode={props.editMode} key={obj.id} text={obj.text}/>
-    )
+    const dispatch = useDispatch()
+    const [selectedId, setSelectedId] = useState(undefined)
+    const onSelectedTask = (id) => {
+        setSelectedId(id)
+    }
+
+    const toActiveEditMode = (props) => {
+        dispatch(changedEditMode(true))
+        console.log("i dispatch action for changed editMode to true")
+    }
+
+    const deactivationEditMode = (props) => {
+        dispatch(changedEditMode(false))
+    }
+
     const addTasksInput = () => {
 
     }
+    if (!props.isAuth) return <Navigate to={"/login"} replace={true}/>
+
+    let taskEl = props.newTasksData.map(obj =>
+        <div onClick={() => onSelectedTask(obj.id)} key={obj.id}>
+            <TaskBlock editMode={props.editMode} text={obj.text} />
+            {selectedId === obj.id &&
+            <div className={s.tasksBtn}>
+                <Button onClick={{deactivationEditMode}} className={s.saveTasksBtn} variant="contained" endIcon={<SendIcon />}>Save</Button>
+                <Button className={s.deleteTasksBtn} variant="outlined" startIcon={<DeleteIcon />}>Delete</Button>
+            </div>
+            }
+        </div>
+    )
+
     return (
         <div className={s.taskListWr}>
             <Button variant="contained" onClick={addTasksInput}>add new Task</Button>
@@ -21,9 +50,11 @@ const TaskListPageContainer = (props) => {
         </div>
     )
 }
+
 const mapStateToProps = (state) => ({
     newTasksData: state.tasksList.newTasksData,
     isAuth: state.registration.isAuth,
-    editMode: state.tasksList.editMode
+    editMode: state.tasksList.editMode,
+
 })
-export default connect(mapStateToProps)(TaskListPageContainer);
+export default connect(mapStateToProps, {changedEditMode})(TaskListPageContainer);
--- src/store/taskList-reducer.js
+++ src/store/taskList-reducer.js
@@ -1,26 +1,40 @@
 const ADD_TASK = "ADD_TASK"
+const CHANGED_EDIT_MODE = "CHANGED_EDIT_MODE"
+
+const addTask = () => ({type:ADD_TASK})
+export const changedEditMode = (mode) => ({type:CHANGED_EDIT_MODE,mode})
+
+
 let initialState = {
     newTasksData: [
-        {id: 1,
-    text: "У попа была собака, он ее любил.Она съела кусок мяса, он ее казнил",
-    color: "",
-    label: "",
-    backgroundColor: ""},
-        {id: 2,
+        {
+            id: 1,
+            text: "У попа была собака, он ее любил.Она съела кусок мяса, он ее казнил",
+            color: "",
+            label: "",
+            backgroundColor: ""
+        },
+        {
+            id: 2,
             text: "У кита была собака, он ее любил.Она съела кусок мяса, он ее казнил",
             color: "",
             label: "",
-            backgroundColor: ""},
-        {id: 3,
+            backgroundColor: ""
+        },
+        {
+            id: 3,
             text: "У кота была собака, он ее любил.Она съела кусок мяса, он ее казнил",
             color: "",
             label: "",
-            backgroundColor: ""},
-        {id: 4,
+            backgroundColor: ""
+        },
+        {
+            id: 4,
             text: "У шута была собака, он ее любил.Она съела кусок мяса, он ее казнил",
             color: "",
             label: "",
-            backgroundColor: ""},
+            backgroundColor: ""
+        },
     ],
     editMode: false
 
@@ -29,8 +43,11 @@ let initialState = {
 const taskListReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TASK: {
+            return {}
+        }
+        case CHANGED_EDIT_MODE: {
             return {
-
+              ...state,editMode: action.mode
             }
         }
         default:
