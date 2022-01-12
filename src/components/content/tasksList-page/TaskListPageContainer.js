3a120aebb62e309ba0e78e9bafd34a3abafbc7c9
marsianca <xmarsiancax@mail.ru>
2022-01-12 16:04:37 +0300

    editMode activeted success to TaskBlock component

--- src/components/content/tasksList-page/TaskListPageContainer.js
+++ src/components/content/tasksList-page/TaskListPageContainer.js
@@ -3,50 +3,22 @@ import {Button} from "@mui/material";
 import {connect, useDispatch} from "react-redux";
 import {Navigate} from "react-router-dom";
 import {changedEditMode} from "../../../store/taskList-reducer";
-import TaskBlock from "./TaskBlock";
-import {useState} from "react";
-import SendIcon from "@material-ui/icons/Send";
-import DeleteIcon from "@material-ui/icons/Delete";
+import TaskBlock from "./taskBlock";
+
+
+
 
 const TaskListPageContainer = (props) => {
-    const dispatch = useDispatch()
-    const [selectedId, setSelectedId] = useState(undefined)
-    const onSelectedTask = (id) => {
-        setSelectedId(id)
-    }
-
-    const toActiveEditMode = (props) => {
-        dispatch(changedEditMode(true))
-        console.log("i dispatch action for changed editMode to true")
-    }
-
-    const deactivationEditMode = (props) => {
-        dispatch(changedEditMode(false))
-    }
+    if (!props.isAuth) return <Navigate  to={"/login"} replace={true}/>
 
     const addTasksInput = () => {
 
     }
-    if (!props.isAuth) return <Navigate to={"/login"} replace={true}/>
-
-    let taskEl = props.newTasksData.map(obj =>
-        <div onClick={() => onSelectedTask(obj.id)} key={obj.id}>
-            <TaskBlock editMode={props.editMode} text={obj.text} />
-            {selectedId === obj.id &&
-            <div className={s.tasksBtn}>
-                <Button onClick={{deactivationEditMode}} className={s.saveTasksBtn} variant="contained" endIcon={<SendIcon />}>Save</Button>
-                <Button className={s.deleteTasksBtn} variant="outlined" startIcon={<DeleteIcon />}>Delete</Button>
-            </div>
-            }
-        </div>
-    )
 
     return (
         <div className={s.taskListWr}>
             <Button variant="contained" onClick={addTasksInput}>add new Task</Button>
-            <div className={s.taskZone}>
-                {taskEl}
-            </div>
+            <TaskBlock newTasksData={props.newTasksData} className={s.taskZone}/>
         </div>
     )
 }
---
+++ src/components/content/tasksList-page/taskBlock.js
@@ -0,0 +1,45 @@
+import s from "./taskBlock.module.css";
+import note from "../../../img/images (2).png";
+import {Button} from "@mui/material";
+import SendIcon from "@material-ui/icons/Send";
+import DeleteIcon from "@material-ui/icons/Delete";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+
+const TaskBlock = (props) => {
+    const dispatch = useDispatch()
+    const [selectedId, setSelectedId] = useState(undefined)
+    const onSelectedTask = (id) => {
+        setSelectedId(id)
+    }
+
+    const toActiveEditMode = (props) => {
+        dispatch(props.changedEditMode(true))
+        console.log("i dispatch action for changed editMode to true")
+    }
+
+    const deactivationEditMode = (props) => {
+        dispatch(props.changedEditMode(false))
+    }
+
+    const taskEl = props.newTasksData.map(obj =>
+        <div onClick={() => onSelectedTask(obj.id)} key={obj.id} className={s.taskBlock}>
+            <img src={note} alt="noteBackground"/>
+            <p>{obj.text}</p>
+            {selectedId === obj.id &&
+            <div className={s.tasksBtn}>
+                <Button onClick={{deactivationEditMode}} className={s.saveTasksBtn} variant="contained"
+                        endIcon={<SendIcon/>}>Save</Button>
+                <Button className={s.deleteTasksBtn} variant="outlined" startIcon={<DeleteIcon/>}>Delete</Button>
+            </div>
+            }
+        </div>
+    )
+
+    return (
+       <div className={s.taskZone}>
+           {taskEl}
+       </div>
+    )
+}
+export default TaskBlock;
