48ac2edec3b710bd9123d0824234b81b3d1e690d
marsianca <xmarsiancax@mail.ru>
2022-02-03 12:29:12 +0300

    added error message for not identical passwords

--- src/components/content/registration-page/RegistrationForm.js
+++ src/components/content/registration-page/RegistrationForm.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./RegistrationForm.module.css"
 import {useForm} from "react-hook-form";
-import {Button, Input, TextField} from "@mui/material";
+import {Button, TextField} from "@mui/material";
 import {setAuthorizationHeader, usersApi} from "../../../api/api";
-import {setErrorsText, setRegistrationData, setRegistrationErrorsText} from "../../../store/registration-reducer";
-import {connect, useDispatch} from "react-redux";
+import {setRegistrationData, setRegistrationErrorsText} from "../../../store/registration-reducer";
+import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
 
 
 const RegistrationForm = (props) => {
+    const [isIdenticalPasswords, setNotIdentical] = useState(true)
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const {register, handleSubmit, formState: {errors}} = useForm()
     const onSubmit = (data) => {
-        usersApi.usersRegistration(data)
-            .then(response => {
-                    let {id, username} = response.data;
-                    let {token} = response.data
-                    dispatch(setRegistrationData({id, username}))
-                    localStorage.setItem("token", token)
-                    setAuthorizationHeader(token)
-                    navigate("/taskList", {replace: true})
-                },error => {
-            let registrationTextError = error.response.data.errors[0]
-            dispatch(setRegistrationErrorsText(registrationTextError))
+        if (data.password === data.repeatPassword) {
+            usersApi.usersRegistration(data)
+                .then(response => {
+                        let {id, username} = response.data;
+                        let {token} = response.data
+                        dispatch(setRegistrationData({id, username}))
+                        localStorage.setItem("token", token)
+                        setAuthorizationHeader(token)
+                        navigate("/taskList", {replace: true})
+                    }, error => {
+                        let registrationTextError = error.response.data.errors[0]
+                        dispatch(setRegistrationErrorsText(registrationTextError))
+                    }
+                )
+        } else {
+            setNotIdentical(false)
         }
-            )
+
     }
     return (
         <div>
@@ -81,7 +87,11 @@ const RegistrationForm = (props) => {
                     <div>Max length exceeded</div>}
                     {errors.repeatPassword && errors.repeatPassword.type === "minLength" &&
                     <div>Min length not reached</div>}
-                    {props.registrationTextError && <div className={s.registrationMessageErrorsSpan}>{props.registrationTextError}</div>}
+                    {props.registrationTextError &&
+                    <div className={s.registrationMessageErrorsSpan}>{props.registrationTextError}</div>}
+                    {!isIdenticalPasswords &&
+                    <div className={s.isIdenticalErrorsSpan}>This passwords not identical</div>
+                    }
                 </div>
                 <div className={s.loginButton}>
                     <Button variant="contained" type="submit">
